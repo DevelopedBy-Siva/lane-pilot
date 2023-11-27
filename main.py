4d1fae767b39effef2f5ecf00125c83e137945e6
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-11-28 00:59:40 +0530

    Covert to grayscale & reduce noise

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import cv2
+import numpy as np
 
-img = cv2.imread("lane.jpg")
-cv2.imshow('Lane', img)
+original_img = cv2.imread("lane.jpg")
+lane_image = np.copy(original_img)
+
+# Convert lane_image to grayscale
+grayscale_image = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+# Filter the grayscale_image to reduce noise
+blur_image = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
+
+cv2.imshow('Lane', blur_image)
 cv2.waitKey(0)
