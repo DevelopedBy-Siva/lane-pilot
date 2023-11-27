062f871b1b44e3851a3b3bd94a3ffbac23d330a9
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-11-28 03:34:59 +0530

    Draw Hough lines on the lane image

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def find_edges_with_canny(image):
     """
-        1. Convert lane_image to grayscale
-        2. Filter the grayscale_image to reduce noise
-        3. Canny Edge detection
+        - Convert lane_image to grayscale
+        - Filter the grayscale_image to reduce noise
+        - Canny Edge detection
 
         :param image: Lane image
         :return: Canny edge detected image
@@ -17,22 +17,37 @@ def find_edges_with_canny(image):
     return canny_image
 
 
-def region_of_interest(image):
+def apply_region_of_interest(canny_image):
     """
-    Mask the image to separate the region
-    :param image: Canny image
-    :return: masked canny image
+    Applies a region of interest mask to a Canny edge-detected image.
+    :param canny_image: Canny edge-detected image
+    :return: Masked Canny image within the specified region of interest
     """
-    image_height = image.shape[0]
-    views = np.array([[(200, image_height), (1100, image_height), (550, 250)]])
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, views, 255)
-    masked_canny = cv2.bitwise_and(image, mask)
-    return masked_canny
+    image_height = canny_image.shape[0]
+    region_vertices = np.array([[(200, image_height), (1100, image_height), (550, 250)]])
+    region_mask = np.zeros_like(canny_image)
+    cv2.fillPoly(region_mask, region_vertices, 255)
+    masked_canny_image = cv2.bitwise_and(canny_image, region_mask)
+    return masked_canny_image
 
 
-def display_lines(image, line):
-    return ""
+def draw_hough_lines(image, hough_lines):
+    """
+        Draws detected Hough lines on a black image.
+        :param image: Lane image
+        :param hough_lines: Hough lines
+        :return: Image with the detected lines drawn on it
+    """
+
+    # Create a black image with the same dimensions as the input image
+    line_image = np.zeros_like(image)
+    if hough_lines is not None:
+        for line in hough_lines:
+            # Extract the coordinates of the two endpoints of the line
+            x1, y1, x2, y2 = line.reshape(4)
+            # Draw a blue line on the 'line_image' using the extracted coordinates with a thickness of 10px
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
 
 
 original_img = cv2.imread("lane.jpg")
@@ -42,12 +57,16 @@ lane_image = np.copy(original_img)
 canny_output = find_edges_with_canny(lane_image)
 
 # Mask canny image
-isolated_region = region_of_interest(canny_output)
+isolated_region = apply_region_of_interest(canny_output)
 
 # Using Hough Line Transform to detect lines in an image.
-lines = cv2.HoughLinesP(isolated_region, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-line_image = display_lines(lane_image, lines)
+detected_lines = cv2.HoughLinesP(isolated_region, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+# Drae lines on the image
+line_output_image = draw_hough_lines(lane_image, detected_lines)
+
+# Combine the original lane image with the image containing detected lines
+blended_image = cv2.addWeighted(lane_image, 0.8, line_output_image, 1, 1)
 
 # Show result
-cv2.imshow('Lane', isolated_region)
+cv2.imshow('Lane', blended_image)
 cv2.waitKey(0)
