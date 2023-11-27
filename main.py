7cff102cf7622c0aeed1142aa88c5719d20c18fd
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-11-28 03:18:34 +0530

    Using Hough tranform, find lines in the image

--- main.py
+++ main.py
@@ -17,12 +17,37 @@ def find_edges_with_canny(image):
     return canny_image
 
 
+def region_of_interest(image):
+    """
+    Mask the image to separate the region
+    :param image: Canny image
+    :return: masked canny image
+    """
+    image_height = image.shape[0]
+    views = np.array([[(200, image_height), (1100, image_height), (550, 250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, views, 255)
+    masked_canny = cv2.bitwise_and(image, mask)
+    return masked_canny
+
+
+def display_lines(image, line):
+    return ""
+
+
 original_img = cv2.imread("lane.jpg")
 lane_image = np.copy(original_img)
 
 # Canny edge detected output
 canny_output = find_edges_with_canny(lane_image)
 
+# Mask canny image
+isolated_region = region_of_interest(canny_output)
+
+# Using Hough Line Transform to detect lines in an image.
+lines = cv2.HoughLinesP(isolated_region, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+
 # Show result
-cv2.imshow('Lane', canny_output)
+cv2.imshow('Lane', isolated_region)
 cv2.waitKey(0)
